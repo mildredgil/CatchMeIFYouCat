a6578ec11fdd54fe81b03f763c4188f9969c6ae9
Mildred <mi.mitel@hotmail.com>
2018-06-18 19:48:51 -0500

    cat movement

---
+++ CatchMeIfYouCat/Assets/Scripts/cat.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cat : MonoBehaviour {
+
+  public float moveSpeed;
+  public float jump;
+  double min;
+  double max;
+	// Use this for initialization
+	void Start () {
+		moveSpeed = 8f;
+    jump = 2f;
+    Vector2 maxVal= Camera.main.ViewportToWorldPoint(new Vector2 (1 , 1 ));
+    Vector2 minVal = Camera.main.ViewportToWorldPoint(new Vector2 (0 , 0 ));
+    min = minVal.x;
+    max = maxVal.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {    
+    if(transform.position.x <= min)
+	    transform.Translate(moveSpeed*(Input.GetAxis("Horizontal") + 1)*Time.deltaTime,0f,0f);
+    else if(transform.position.x >= max)
+      transform.Translate(moveSpeed*(Input.GetAxis("Horizontal") - 1)*Time.deltaTime,0f,0f);
+    else
+      transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,0f);
+    
+    if(Input.GetKeyDown("up")) {
+      transform.Translate(0f,jump + 0f,0f);
+    }
+    /*    
+    if(Input.GetKeyDown("space")) {
+      GameObject bulletpos = (GameObject)Instantiate (Player);
+      bulletpos.transform.position = bullet.transform.position;
+    }*/
+	}
+}
+  
---
+++ CatchMeIfYouCat/Assets/Scripts/shoot.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour {
+
+  float speed;
+	// Use this for initialization
+	void Start () {
+		speed = 8f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    if(Input.GetKeyDown("space")) {
+      shootBullet();
+    }
+	}
+
+  void shootBullet() {
+    Vector2 position = transform.position;
+    position = new Vector2(position.x + speed * Time.deltaTime, position.y);
+    //Debug.Log(position.y);
+    transform.position = position;
+
+    Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2 (1 , 1));
+
+    if(transform.position.x > max.x) {
+      Destroy(gameObject);
+    }
+
+    //yield return new WaitForSeconds(5);
+    Destroy(gameObject);
+  }
+}
+
