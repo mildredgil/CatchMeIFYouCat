c68b60cf24564278b557ef6ccf6adb9cd28a74ef
Mildred <mi.mitel@hotmail.com>
2018-06-19 22:11:54 -0500

    mvp

--- CatchMeIfYouCat/Assets/Scripts/cat.cs
+++ CatchMeIfYouCat/Assets/Scripts/cat.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class cat : MonoBehaviour {
-
+  public GameObject bullet;
+  public GameObject Player;
+  
   public float moveSpeed;
   public float jump;
   double min;
   double max;
+  bool isGrounded;
 	// Use this for initialization
 	void Start () {
 		moveSpeed = 8f;
-    jump = 2f;
-    Vector2 maxVal= Camera.main.ViewportToWorldPoint(new Vector2 (1 , 1 ));
+    jump = 0;
+    isGrounded = false;
+    Vector2 maxVal = Camera.main.ViewportToWorldPoint(new Vector2 (1 , 1 ));
     Vector2 minVal = Camera.main.ViewportToWorldPoint(new Vector2 (0 , 0 ));
     min = minVal.x;
     max = maxVal.x;
@@ -20,21 +24,39 @@ public class cat : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {    
+    move();
+    jumping();
+    shoot();
+	}
+  void move() {
     if(transform.position.x <= min)
-	    transform.Translate(moveSpeed*(Input.GetAxis("Horizontal") + 1)*Time.deltaTime,0f,0f);
+	    transform.Translate(moveSpeed*(Input.GetAxis("Horizontal") + 1)*Time.deltaTime,jump + 0f,0f);
     else if(transform.position.x >= max)
-      transform.Translate(moveSpeed*(Input.GetAxis("Horizontal") - 1)*Time.deltaTime,0f,0f);
+      transform.Translate(moveSpeed*(Input.GetAxis("Horizontal") - 1)*Time.deltaTime,jump + 0f,0f);
     else
-      transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,0f);
-    
-    if(Input.GetKeyDown("up")) {
-      transform.Translate(0f,jump + 0f,0f);
-    }
-    /*    
+      transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime,jump + 0f,0f);
+//      Debug.Log(Input.GetAxis("Horizontal"));
+  }
+
+  void shoot() {
     if(Input.GetKeyDown("space")) {
       GameObject bulletpos = (GameObject)Instantiate (Player);
       bulletpos.transform.position = bullet.transform.position;
-    }*/
-	}
+    }
+  }
+
+  void jumping() {
+    if(Input.GetKeyDown("up") && isGrounded) {
+      jump = 2;
+      isGrounded = false;
+    } else {
+      jump = 0;
+    } 
+  }
+  void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.tag == "floor" && isGrounded == false) {
+      isGrounded = true;
+		}
+  }
 }
   
--- CatchMeIfYouCat/Assets/Scripts/shoot.cs
+++ CatchMeIfYouCat/Assets/Scripts/shoot.cs
@@ -3,35 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class shoot : MonoBehaviour {
-
-  float speed;
+float speed;
+float distance;
 	// Use this for initialization
 	void Start () {
-		speed = 8f;
+		speed = 15f;
+    distance = transform.position.x + 3;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+    Vector2 position = transform.position;
+    position = new Vector2(position.x + speed * Time.deltaTime, position.y);
+    
+    transform.position = position;
 
-    if(Input.GetKeyDown("space")) {
-      shootBullet();
+    Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2 (1 , 1 ));
+    
+    if(transform.position.x > max.x || position.x >= distance) {
+      Destroy(gameObject);
     }
 	}
 
-  void shootBullet() {
-    Vector2 position = transform.position;
-    position = new Vector2(position.x + speed * Time.deltaTime, position.y);
-    //Debug.Log(position.y);
-    transform.position = position;
-
-    Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2 (1 , 1));
-
-    if(transform.position.x > max.x) {
-      Destroy(gameObject);
-    }
-
-    //yield return new WaitForSeconds(5);
-    Destroy(gameObject);
+  void OnCollisionEnter2D(Collision2D col) {
+    Debug.Log("here");
+		if(col.gameObject.tag == "rat") {
+      Destroy(col.gameObject);
+		}
   }
 }
-
