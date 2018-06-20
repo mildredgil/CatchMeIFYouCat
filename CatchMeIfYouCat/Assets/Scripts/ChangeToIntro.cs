471fe02b94a9e79ae672d148809f5a4b853d9df2
Marcela Rico <marce.rr@hotmail.com>
2018-06-19 20:02:48 -0500

    marce changes

---
+++ CatchMeIfYouCat/Assets/Scripts/ChangeToIntro.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeToIntro : MonoBehaviour
+{
+
+    public void ChangeToLevelOne(string scene)
+    {
+        SceneManager.LoadScene("Level1");
+    }
+}
