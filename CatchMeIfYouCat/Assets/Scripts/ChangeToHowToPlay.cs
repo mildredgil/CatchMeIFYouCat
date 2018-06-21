2068df4a0e4f6da7e2f8605dda8dfe4bca067f35
Marcela Rico <marce.rr@hotmail.com>
2018-06-21 12:46:16 -0500

    marce changes

---
+++ CatchMeIfYouCat/Assets/Scripts/ChangeToHowToPlay.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeToHowToPlay : MonoBehaviour
+{
+
+    public void ChangeToHTP(string scene)
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
+}
