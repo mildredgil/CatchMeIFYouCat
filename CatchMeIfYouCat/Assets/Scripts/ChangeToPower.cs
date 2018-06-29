31193f5d372f76800e02ba1bd1cb722e4d3d7d0f
Marcela Rico <marce.rr@hotmail.com>
2018-06-28 23:08:06 -0500

    nueva scene

---
+++ CatchMeIfYouCat/Assets/Scripts/ChangeToPower.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeToPower : MonoBehaviour
+{
+
+    public void ChangePower(string scene)
+    {
+        SceneManager.LoadScene("Power");
+    }
+}
